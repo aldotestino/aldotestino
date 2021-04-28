141d12c883e3233815e94ffda0c99db540394475
aldotestino <aldo.testino@libero.it>
2021-04-29 00:24:30 +0200

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { AcademicCapIcon, BriefcaseIcon, MailIcon } from '@heroicons/react/outline';
+import { AcademicCapIcon, CodeIcon, MailIcon } from '@heroicons/react/outline';
+import clsx from 'clsx';
 import React, { useState } from 'react';
 import { useNotify } from '../components/Notification';
 import SEO from '../components/SEO';
@@ -17,8 +18,8 @@ const jobs = [
     icon: AcademicCapIcon
   },
   {
-    top: 'Freelancer',
-    icon: BriefcaseIcon
+    top: 'Full Stack Developer',
+    icon: CodeIcon
   }
 ];
 
@@ -30,6 +31,13 @@ const contacts = [
     link: 'https://twitter.com/aldotestino4',
     icon: <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 24 24">
       <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
+    </svg>
+  },
+  {
+    text: 'Discord',
+    link: 'https://discord.gg/N3hKAKVZ',
+    icon: <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+      <path d="M19.54 0c1.356 0 2.46 1.104 2.46 2.472v21.528l-2.58-2.28-1.452-1.344-1.536-1.428.636 2.22h-13.608c-1.356 0-2.46-1.104-2.46-2.472v-16.224c0-1.368 1.104-2.472 2.46-2.472h16.08zm-4.632 15.672c2.652-.084 3.672-1.824 3.672-1.824 0-3.864-1.728-6.996-1.728-6.996-1.728-1.296-3.372-1.26-3.372-1.26l-.168.192c2.04.624 2.988 1.524 2.988 1.524-1.248-.684-2.472-1.02-3.612-1.152-.864-.096-1.692-.072-2.424.024l-.204.024c-.42.036-1.44.192-2.724.756-.444.204-.708.348-.708.348s.996-.948 3.156-1.572l-.12-.144s-1.644-.036-3.372 1.26c0 0-1.728 3.132-1.728 6.996 0 0 1.008 1.74 3.66 1.824 0 0 .444-.54.804-.996-1.524-.456-2.1-1.416-2.1-1.416l.336.204.048.036.047.027.014.006.047.027c.3.168.6.3.876.408.492.192 1.08.384 1.764.516.9.168 1.956.228 3.108.012.564-.096 1.14-.264 1.74-.516.42-.156.888-.384 1.38-.708 0 0-.6.984-2.172 1.428.36.456.792.972.792.972zm-5.58-5.604c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332.012-.732-.54-1.332-1.224-1.332zm4.38 0c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332 0-.732-.54-1.332-1.224-1.332z"/>
     </svg>
   },
   {
@@ -107,7 +115,7 @@ export default function Home() {
               <section className="mt-12">
                 <form onSubmit={handleSubmit}>
                   <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="selena@gmail.com" className="transition focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none w-full bg-transparent py-2 px-4 text-lg placeholder-gray-500 shadow-md border-2 border-gray-500 rounded-lg"/>
+                    <input value={email} type="email" required onChange={(e) => setEmail(e.target.value)} placeholder="selena@gmail.com" className="transition focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none w-full bg-transparent py-2 px-4 text-lg placeholder-gray-500 shadow-md border-2 border-gray-500 rounded-lg"/>
                     <button type="submit" className="text-gray-50 transition whitespace-nowrap py-2 px-4 text-lg bg-indigo-500 rounded-lg w-full sm:w-auto hover:bg-indigo-600 focus:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-gray-900 shadow-md focus:ring-offset-2 focus:ring-indigo-400">
                       Contact me
                     </button>
@@ -127,8 +135,11 @@ export default function Home() {
             <h4 className="uppercase text-lg font-medium text-indigo-500 tracking-wider">Stack</h4>
             <h3 className="text-3xl font-semibold mt-2">Technologies that i like to use.</h3>
           </header>
-          <section className="mt-10 grid grid-rows-3 gap-5 md:grid-cols-2 md:grid-rows-3 md:gap-10 lg:grid-cols-3 lg:grid-rows-2 lg:gap-20">
-            {stack.map((s, i) => <StackIcon key={i} text={s} />)}
+          <section className="mt-10 grid grid-rows-3 gap-4 sm:grid-cols-2 sm:grid-rows-3 md:gap-8 lg:grid-cols-3 lg:grid-rows-2 lg:gap-16">
+            {stack.map((s, i) => 
+              <div key={i} className={clsx(i > 2 && 'hidden sm:block')}>
+                <StackIcon text={s} />
+              </div>)}
           </section>
         </div>
 
