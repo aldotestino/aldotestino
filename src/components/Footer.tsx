d627cfeb675f0d3450e5034290ce61e53e886b06
aldotestino <aldotestino4@gmail.com>
2024-05-03 13:33:00 +0200

    setup nextjs14 with bun

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="text-gray-500 text-center">
-      <p>© {(new Date()).getFullYear()} Aldo Testino. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- src/lib/types.ts
+++
@@ -1,3 +0,0 @@
-import { sections } from './data';
-
-export type SectionName = (typeof sections)[number]['name']
