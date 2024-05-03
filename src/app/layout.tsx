547e46b63f3e7b5aee90df6befe1797c20b1d9fe
aldotestino <aldotestino4@gmail.com>
2024-05-03 13:44:27 +0200

    setup shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] });
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 const title = 'aldotestino';
 const description = 'Software Engineering student & Full Stack Developer';
@@ -36,7 +40,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn('min-h-screen bg-background font-sans antialiased dark', fontSans.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
